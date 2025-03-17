26e8dc06ac42311eb1a4a6392c804bfa94e88636
santana <oliveirasantanavinicius@gmail.com>
2025-03-17 16:21:05 -0300

    instalado nodemon
    instalação do nodemon e anotações

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 //rota com status 200 OK
 app.get('/', (req, res) => {
-    res.status(200).send('Requisição bem-sucedida!');
+    res.status(200).send('Requisição bem-sucedida HOJE!');
 });
 
 //rota com status 404 NOT FOUND
@@ -20,3 +20,12 @@ app.get('/falha', (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
+/*
+    inicia com "npm init -y"
+    instala "npm install express"
+    instalado nodemon como depeendencia de desenvlvimento
+    npm install --save-dev nodemon
+
+    adiciona em package.json -> "dev": "nodemon server.js"
+*/
