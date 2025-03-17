99bcfd0bd86aa1fcc3a5a96e747e0e636338be8a
santana <oliveirasantanavinicius@gmail.com>
2025-03-17 16:23:27 -0300

    anotação

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.listen(port, () => {
 
 /*
     inicia com "npm init -y"
-    instala "npm install express"
-    instalado nodemon como depeendencia de desenvlvimento
+    instala "npm install express" - serverzinho local, tipo o xampp q roda o apache
+    instalado nodemon como depeendencia de desenvlvimento - monitora o node
     npm install --save-dev nodemon
 
     adiciona em package.json -> "dev": "nodemon server.js"
