570f4efbf14c88d5c8723dae3fbd58bd471e30b0
santana <oliveirasantanavinicius@gmail.com>
2025-05-12 12:57:08 -0300

    aula 1 - java
    tudo do outro professor esta como (OLD)

---
+++ exercicios/aula1/ex1.java
@@ -0,0 +1,9 @@
+//package aula1;
+
+public class ex1 {
+    //psvm + tab
+    public static void main(String[] args){
+        System.out.println("Vinícius Santana");
+        System.out.println("Rua APC, 365");
+    }
+}
---
+++ exercicios/aula1/ex2.java
@@ -0,0 +1,7 @@
+//package aula1;
+
+public class ex2{
+    public static void main(String[] args){
+        
+    }
+}
